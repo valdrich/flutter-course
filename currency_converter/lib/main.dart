fd7850d935f0211ff7283cd2b03f3d694137741a
Poliane Ketterine Valdrich <polianevaldrich@gmail.com>
2019-08-31 13:14:28 -0300

    Currency Converter

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -1,17 +1,148 @@
 import "package:flutter/material.dart";
 import "package:http/http.dart" as http;
-import "package:async/async.dart";
+import "dart:async";
 import "dart:convert";
-
 
 const request = "https://api.hgbrasil.com/finance?key=232aa44e";
 
-void main() async{ //informing that main function is a async
-
-  http.Response response = await http.get(request); //await: you'll wait for that response (sync).
-  print(json.decode(response.body)["results"]);
-
+void main() async {
+  //informing that main function is a async
   runApp(MaterialApp(
-    home: Container(),
-  ));
+      home: Home(),
+      theme: ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.amberAccent,
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white))))));
+}
+
+Future<Map> getData() async {
+  http.Response response =
+      await http.get(request); //await: you'll wait for that response (sync).
+  return json.decode(response.body);
+} //will return a map into the future
+
+//inform "stful" and tab to create this automatically
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _clearAll() {
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+  }
+
+  void _realChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    euroController.text = (euro * this.euro / euro).toStringAsFixed(2);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //bar on the top
+      backgroundColor: Colors.black38,
+      appBar: AppBar(
+        title: Text("\$ Converter \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+          //build the screen after loading all data
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                    child: Text(
+                  "Loading data...",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ));
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text(
+                    "Error on loading data...",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ));
+                } else {
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on,
+                            size: 150.0, color: Colors.amber),
+                        buildTextField(
+                            "Reais", "R\$ ", realController, _realChanged),
+                        Divider(), //separate the components
+                        buildTextField(
+                            "Dolar", "US\$ ", dolarController, _dolarChanged),
+                        Divider(),
+                        buildTextField(
+                            "Euro", "€ ", euroController, _euroChanged),
+                      ],
+                    ),
+                  );
+                }
+            }
+          }),
+    );
+  }
+}
+
+Widget buildTextField(
+    String label, String prefix, TextEditingController c, Function f) {
+  return TextField(
+    controller: c,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.white, fontSize: 25.0),
+    onChanged: f,
+    keyboardType: TextInputType.numberWithOptions(decimal: true), //input only numbers
+  );
 }
